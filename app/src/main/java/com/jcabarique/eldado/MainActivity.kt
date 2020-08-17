a9ff1dfbc31fb1b25dffc912970023eb8f04c9cb
Juan Carlos Hoyos <jcabarique@uninorte.edu.co>
2020-08-17 12:52:01 -0500

    Se termino el juego del dado

--- app/src/main/java/com/jcabarique/eldado/MainActivity.kt
+++ app/src/main/java/com/jcabarique/eldado/MainActivity.kt
@@ -11,18 +11,12 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    // Views
-    val turno: TextView = findViewById(R.id.textTurno)
-    val listGanadores: ListView = findViewById(R.id.listGanadores)
-    val listJugadores: ListView = findViewById(R.id.listJugadores)
 
     // Variables de funcionamiento
     var jugador: Int = 1
-    val arrayLanzamientos: ArrayList<Int> = arrayListOf<Int>()
-    val arrayGanadores: ArrayList<String> = arrayListOf<String>()
-    var arrayAdapterJugadores: ArrayAdapter<Int> = ArrayAdapter<Int>(this, android.R.layout.simple_list_item_1, arrayLanzamientos)
-    var arrayAdapterGanadores: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayGanadores)
     var terminar: Boolean = false
+    var tuplas = arrayOf(0,0,0,0,0)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +26,13 @@ class MainActivity : AppCompatActivity() {
 
 
     fun onClickLanzar(view: View) {
-        if(!terminar){
-            turno.text = "Turno jugador: " + jugador.toString()
+        val turno: TextView = findViewById(R.id.textTurno)
+        if(jugador !== 6){
             val dado = Dado(6)
             val valDado = dado.lanzar()
-            arrayLanzamientos.add(valDado)
+
+            // Guarda el valor de cada lanzamiento
+            tuplas.set(jugador - 1, valDado)
 
             when (valDado){
                 1 -> imageView.setImageResource(R.drawable.dice_1)
@@ -46,33 +42,31 @@ class MainActivity : AppCompatActivity() {
                 5 -> imageView.setImageResource(R.drawable.dice_5)
                 6 -> imageView.setImageResource(R.drawable.dice_6)
             }
+            // Muesta el turno en pantalla
             jugador+=1
+            turno.text = "Turno jugador: " + jugador.toString()
+        }
+
+        if(jugador === 6){
+            val resultadoText: TextView = findViewById(R.id.textView)
+            var turnos: String = "";
+
+           for (item in tuplas){
+                turnos+= item.toString() + '-'
+            }
+
+            resultadoText.text = turnos
+            turno.text = "Game over"
         }
     }
 
-    fun onClickTerminar(view: View) {
-        var mayor: Int = 0
-        for ((i, value) in arrayLanzamientos.withIndex()){
-            if (value > mayor) {
-                mayor = value
-            }
-        }
-        // Añade cual o cuales jugadores tienen el numero de dado mayor
-        for ((i, value) in arrayLanzamientos.withIndex()){
-            if (value == mayor) {
-               arrayGanadores.add("Jugador " + (i + 1).toString())
-            }
-        }
-
-        listJugadores.adapter = arrayAdapterJugadores
-        listGanadores.adapter = arrayAdapterGanadores
-
-        terminar = true
-    }
 
     fun onClickReiniciar(view: View) {
         terminar = false
-        arrayLanzamientos.clear()
-        arrayGanadores.clear()
+        val turno: TextView = findViewById(R.id.textTurno)
+        val resultadoText: TextView = findViewById(R.id.textView)
+        resultadoText.text = ""
+        turno.text = "Turno jugador: 1 "
+        jugador = 2
     }
 }
